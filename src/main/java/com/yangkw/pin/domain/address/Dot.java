f4a8ffcf26e4b88f3b224813942eaf4708e37a06
kevin <kw89@qq.com>
2019-04-19 22:04:45 +0800

    1

--- src/main/java/com/yangkw/pin/domain/address/Dot.java
+++ src/main/java/com/yangkw/pin/domain/address/Dot.java
@@ -2,6 +2,7 @@ package com.yangkw.pin.domain.address;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -12,6 +13,7 @@ import java.io.Serializable;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Dot implements Serializable {
     private static final long serialVersionUID = 779891169234313335L;
     /**
