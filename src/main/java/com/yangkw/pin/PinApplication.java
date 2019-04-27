ab0cb6ba317ad563c6b61cb0ae663396f5679b3a
kevin <kw89@qq.com>
2019-04-27 21:30:34 +0800

    admin

--- src/main/java/com/yangkw/pin/PinApplication.java
+++ src/main/java/com/yangkw/pin/PinApplication.java
@@ -1,11 +1,16 @@
 package com.yangkw.pin;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 /**
@@ -15,11 +20,38 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 @EnableCaching
 @EnableAsync
 @EnableWebSocket
-public class PinApplication {
+@EnableAdminServer
+public class PinApplication extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AdminServerProperties adminServerProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(PinApplication.class, args);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String adminContextPath = adminServerProperties.getContextPath();
+
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(
+                        adminContextPath + "/instances",
+                        adminContextPath + "/actuator/**"
+                );
+
+    }
 }
 
