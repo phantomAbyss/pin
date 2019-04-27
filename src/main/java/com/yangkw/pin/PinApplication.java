c67a879b5c94367edac5a3e62f963cacefb8dded
kevin <kw89@qq.com>
2019-04-27 23:56:23 +0800

    1

--- src/main/java/com/yangkw/pin/PinApplication.java
+++ src/main/java/com/yangkw/pin/PinApplication.java
@@ -1,16 +1,9 @@
 package com.yangkw.pin;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 /**
@@ -20,38 +13,11 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 @EnableCaching
 @EnableAsync
 @EnableWebSocket
-@EnableAdminServer
-public class PinApplication extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private AdminServerProperties adminServerProperties;
+public class PinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PinApplication.class, args);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        String adminContextPath = adminServerProperties.getContextPath();
-
-        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-        successHandler.setTargetUrlParameter("redirectTo");
-        successHandler.setDefaultTargetUrl(adminContextPath + "/");
-
-        http.authorizeRequests()
-                .antMatchers(adminContextPath + "/assets/**").permitAll()
-                .antMatchers(adminContextPath + "/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
-                .logout().logoutUrl(adminContextPath + "/logout").and()
-                .httpBasic().and()
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers(
-                        adminContextPath + "/instances",
-                        adminContextPath + "/actuator/**"
-                );
-
-    }
 }
 
